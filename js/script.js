ff3c26c4e1aad16b0b12a8b54086056dc0aad217
Felipe Garcia <felipegarcia1402@gmail.com>
2023-06-08 21:44:23 -0300

    GS-002 Implemented header design and add responsiveness

--- js/script.js
+++ js/script.js
@@ -14,6 +14,26 @@ function create_tab_active_event()
     });
 }
 
+function create_toggle_theme_event()
+{
+    var modeToggleBtn = document.getElementById('theme-toggle');
+    var icon = document.getElementById('theme-toggle-icon');
+
+    modeToggleBtn.addEventListener('click', () => {
+        var root = document.documentElement;
+        var isDarkMode = root.classList.contains('dark-mode');
+
+        if (isDarkMode) {
+            icon.className = 'fas fa-moon';
+            root.classList.remove('dark-mode');
+        } else {
+            icon.className = 'fas fa-sun';
+            root.classList.add('dark-mode');
+        }
+    });
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     create_tab_active_event();
+    create_toggle_theme_event();
 });
